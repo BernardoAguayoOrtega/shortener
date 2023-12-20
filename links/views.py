b6f67a69bbd995c50d4d8c0760115277e7b81015
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2023-12-20 08:55:01 -0600

    Update index.html and views.py

--- links/views.py
+++ links/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import render
 
+from .models import Link
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, "links/index.html", {})
+    links = Link.objects.all()
+    
+    context = {
+        "links": links,
+    }
+    
+    return render(request, "links/index.html", context)
