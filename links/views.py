2f5670d398f6bbe21357c7f3a61d496df47d352f
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2023-12-29 10:42:08 -0600

    Add LinkForm and update add_link view

--- links/views.py
+++ links/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Link
 
+from .forms import LinkForm
+
 # Create your views here.
 
 
@@ -21,5 +23,17 @@ def root_link(request, link_slug):
 
 
 def add_link(request):
-    print(request.POST)
-    return render(request, "links/add_link.html")
+    
+    if request.method == "POST":
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            link = form.save()
+            return redirect("home")
+    else:
+        form = LinkForm()
+        
+    context = {
+        "form": form,
+    }
+    
+    return render(request, "links/add_link.html", context)
